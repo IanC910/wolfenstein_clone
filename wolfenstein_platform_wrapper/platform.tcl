91b938790e527543dbd47a11650122fee769ebd6
IanC910 <81271333+IanC910@users.noreply.github.com>
2024-03-17 23:27:28 -0700

    Update platform project with new hw bitstream

--- wolfenstein_platform_wrapper/platform.tcl
+++ wolfenstein_platform_wrapper/platform.tcl
@@ -32,3 +32,6 @@ platform generate
 platform active {wolfenstein_platform_wrapper}
 platform config -updatehw {C:/Users/Ian/Projects/school/ensc452/wolfenstein_clone/wolfenstein_platform/wolfenstein_platform_wrapper.xsa}
 platform generate
+platform active {wolfenstein_platform_wrapper}
+platform config -updatehw {C:/Users/Ian/Projects/school/ensc452/wolfenstein_clone/wolfenstein_platform/wolfenstein_platform_wrapper.xsa}
+platform generate -domains 
--- wolfenstein_platform_wrapper/ps7_cortexa9_0/standalone_ps7_cortexa9_0/bsp/ps7_cortexa9_0/include/xparameters.h
+++ wolfenstein_platform_wrapper/ps7_cortexa9_0/standalone_ps7_cortexa9_0/bsp/ps7_cortexa9_0/include/xparameters.h
@@ -249,42 +249,32 @@
 #define XPAR_ZED_AUDIO_CTRL_0_HIGHADDR 0x43C1FFFF
 
 
+/* Definitions for peripheral BTN_INTERRUPT_GEN_0 */
+#define XPAR_BTN_INTERRUPT_GEN_0_BASEADDR 0x43C20000
+#define XPAR_BTN_INTERRUPT_GEN_0_HIGHADDR 0x43C2FFFF
+
+
 /******************************************************************/
 
 /* Definitions for driver GPIO */
-#define XPAR_XGPIO_NUM_INSTANCES 2
-
-/* Definitions for peripheral AXI_GPIO_BTNS */
-#define XPAR_AXI_GPIO_BTNS_BASEADDR 0x41200000
-#define XPAR_AXI_GPIO_BTNS_HIGHADDR 0x4120FFFF
-#define XPAR_AXI_GPIO_BTNS_DEVICE_ID 0
-#define XPAR_AXI_GPIO_BTNS_INTERRUPT_PRESENT 1
-#define XPAR_AXI_GPIO_BTNS_IS_DUAL 0
-
+#define XPAR_XGPIO_NUM_INSTANCES 1
 
 /* Definitions for peripheral AXI_GPIO_I2C_ADDR */
 #define XPAR_AXI_GPIO_I2C_ADDR_BASEADDR 0x41210000
 #define XPAR_AXI_GPIO_I2C_ADDR_HIGHADDR 0x4121FFFF
-#define XPAR_AXI_GPIO_I2C_ADDR_DEVICE_ID 1
+#define XPAR_AXI_GPIO_I2C_ADDR_DEVICE_ID 0
 #define XPAR_AXI_GPIO_I2C_ADDR_INTERRUPT_PRESENT 0
 #define XPAR_AXI_GPIO_I2C_ADDR_IS_DUAL 0
 
 
 /******************************************************************/
 
-/* Canonical definitions for peripheral AXI_GPIO_BTNS */
-#define XPAR_GPIO_0_BASEADDR 0x41200000
-#define XPAR_GPIO_0_HIGHADDR 0x4120FFFF
-#define XPAR_GPIO_0_DEVICE_ID XPAR_AXI_GPIO_BTNS_DEVICE_ID
-#define XPAR_GPIO_0_INTERRUPT_PRESENT 1
-#define XPAR_GPIO_0_IS_DUAL 0
-
 /* Canonical definitions for peripheral AXI_GPIO_I2C_ADDR */
-#define XPAR_GPIO_1_BASEADDR 0x41210000
-#define XPAR_GPIO_1_HIGHADDR 0x4121FFFF
-#define XPAR_GPIO_1_DEVICE_ID XPAR_AXI_GPIO_I2C_ADDR_DEVICE_ID
-#define XPAR_GPIO_1_INTERRUPT_PRESENT 0
-#define XPAR_GPIO_1_IS_DUAL 0
+#define XPAR_GPIO_0_BASEADDR 0x41210000
+#define XPAR_GPIO_0_HIGHADDR 0x4121FFFF
+#define XPAR_GPIO_0_DEVICE_ID XPAR_AXI_GPIO_I2C_ADDR_DEVICE_ID
+#define XPAR_GPIO_0_INTERRUPT_PRESENT 0
+#define XPAR_GPIO_0_IS_DUAL 0
 
 
 /******************************************************************/
@@ -356,12 +346,11 @@
 /******************************************************************/
 
 /* Definitions for Fabric interrupts connected to ps7_scugic_0 */
-#define XPAR_FABRIC_AXI_GPIO_BTNS_IP2INTC_IRPT_INTR 61U
+#define XPAR_FABRIC_BTN_INTERRUPT_GEN_0_INTERRUPT_INTR 61U
 
 /******************************************************************/
 
 /* Canonical definitions for Fabric interrupts connected to ps7_scugic_0 */
-#define XPAR_FABRIC_GPIO_0_VEC_ID XPAR_FABRIC_AXI_GPIO_BTNS_IP2INTC_IRPT_INTR
 
 /******************************************************************/
 
