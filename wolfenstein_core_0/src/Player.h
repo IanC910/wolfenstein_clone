e51ade9e16289191a5e4da749d88ba9635d8fcc2
IanC910 <81271333+IanC910@users.noreply.github.com>
2024-03-22 15:15:54 -0700

    Create ObjectWithPosition class for inheritance purposes. Create Enemy class skeleton

--- wolfenstein_core_0/src/Player.h
+++ wolfenstein_core_0/src/Player.h
@@ -4,25 +4,20 @@
 
 #include <math.h>
 
-class Player {
+#include "ObjectWithPosition.h"
+
+class Player : public ObjectWithPosition {
 
 public:
 	float getAngle();
-	float getPositionX();
-	float getPositionY();
 	int getHealth();
 
 	void setAngle(float angle);
-	void setPositionX(float x);
-	void setPositionY(float y);
 	void setHealth(int health);
 
 private:
 	float angle = 0;
-	float positionX = 0;
-	float positionY = 0;
 	int health = 0;
 };
-
 
 #endif
